2179f62d996a7896fac7169038bba4c56ab8bc8a
Leung Sze Chung <oscarleung2007@gmail.com>
2026-04-22 21:33:14 +0800

    Expand resourceData with additional topics and categories

--- japanese-tutorial-pdf-data.js
+++ japanese-tutorial-pdf-data.js
@@ -1,7 +1,7 @@
 const resourceData = [
     {
         category: "初級日文N5研習班-初班",
-        categoryId: "jp-n5-tutorial",
+        categoryId: "jp-n5-tutorial-1",
         topics: [
             {
                 title: "Ch.0 假名與發音(1)",
@@ -45,6 +45,207 @@ const resourceData = [
                 title: "Ch.5 片假名",
                 files: [
                     { desc: "課堂筆記", url: "https://drive.google.com/file/d/1pQDFY1Tpvj4RSNU-pKc0edUZJH1UukDI/view?usp=sharing" },
+                ]
+            },
+            {
+                title: "Ch.6 數字及寒暄語",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1VVeSp9k4xl68LOUITHDCGrW79bfqJ7n4/view?usp=drive_link" },
+                ]
+            },
+            {
+                title: "Ch.7 動詞(1)",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1aU-_5ZSpQJCU6LEi4F7mz7UBD-fKpVn9/view?usp=sharing" },
+                ]
+            },
+            {
+                title: "Ch.9 名詞(1)",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1P3iq_HBHrskogSrjcKr1gEykRE1--gzw/view?usp=sharing" },
+                ]
+            },
+            {
+                title: "Ch.10 助詞へ、で、と",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1tFy-4VSepi7gncchNyEOMZwaCxTYqwG6/view?usp=sharing" },
+                ]
+            },
+            {
+                title: "Ch.11 助詞を、で",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1-EKVFfJ6_GQxhEE2ri4ASs8weSQ04mpY/view?usp=sharing" },
+                ]
+            },
+            {
+                title: "Ch.12 ポルトガル語から日本語への借用の単語",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1dcYQ2Pv8EwdD6cK4rnHEMMv__bhl2aTB/view?usp=sharing" },
+                ]
+            },
+            {
+                title: "Ch.13 動詞(2)",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1BdZ_PRx7z3idMj0tj11pinWim3dhVJTR/view?usp=sharing" },
+                ]
+            },
+            {
+                title: "Ch.14 助詞に",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1m_oLFAIqmZEQQS0sZmaL4zTPXUIXjXa_/view?usp=sharing" },
+                ]
+            },
+            {
+                title: "Ch.15 ローマ字入力の使い方",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/19wCZ5rVVKBSmYhTR_yYGsbnS0Kb_0LnI/view?usp=sharing" },
+                ]
+            }
+        ]
+    },
+    
+    {
+        category: "初級日文N5研習班-中級",
+        categoryId: "jp-n5-tutorial-2",
+        topics: [
+            {
+                title: "Ch.17 存在文及數量詞",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1OUlAuPtuNWXM-lFNuTzNsZLchGBZfvRz/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.18 形容詞基本用法",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1ZHiKGZXWp4clYvXkSrgQlPpnvXpcTKhH/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.19 動詞(3)-辞書形",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1yW1S5ANLHwMjpiAwFnp5N73nAvqDZobK/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.21 助詞に(2)",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1GSEpppBmMaX-6j0YU-c-00se-2vXcwsl/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.22A 動詞(5A)-て形",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1tAIWXAoo8Bg45woCGM-wBCplz4n3Fbt5/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.22B 動詞(5B)-て形",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1f3HYNTms8R6E87j5Ltn0Z0p2wWzrccL0/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.22C 動詞(5C)-て形",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/10915AOtowXo7Zl8VmQf4fD9Pm5szJf20/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.23 形容詞(2)",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1fyvpEHq2bkLwP-zM5wgeYzwkQC8s2jCt/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.24A 動詞(6A)-ない形",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1bQbiXnvjhRu61q7sEy2LCfgjgAwKGWVh/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.24B 動詞(6B)-ない形",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1mwb9caCfc6l9KZslLgrOtgekS_-MZRHm/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.25 動詞(7)た形",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1fXJUh87tzrT4aIV6DbfYqKQ6EKQZlrwi/view?usp=sharing" }
+                ]
+            },
+            
+        ]
+    },
+    {
+        category: "初級日文N5研習班-高級",
+        categoryId: "jp-n5-tutorial-3",
+        topics: [
+            {
+                title: "Ch.26 經驗文",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1dWB_CbEDXkCKR8g20UfBpzn0NHgO181r/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.27 練習問題",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/19bU2ytuh5mHJu7AM6J2Oa6pKMMBZK6Pi/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.28 連體修飾",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1K5dCWzMpoO4QFk4-7HAFMX8x8XVqzUeE/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.29 動詞(8)可能形",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1Jn4W1x8y07Z2DrhnYPWswLh117ZpCAnK/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.30 動詞(9)意向形",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1KlfndCuX29h8cDuQbhlO7FatB4-szmqD/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.31 直接話法、間接話法",
+                files: [
+                    { desc: "課堂筆記 V1", url: "https://drive.google.com/file/d/1AH3qXXBkVM5wz95WpPnxNfQ0v40mt2JI/view?usp=sharing" },
+                    { desc: "課堂筆記 V2", url: "https://drive.google.com/file/d/1QmfaOTbP0akuQpoC_EoJgkjTjI6el4YO/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.32 動作の順序・前後",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1lik8GNc8xllFSLaoKMqVjNuAoCbGANXm/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.33 地理学",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1MSGhNhBf5DwYnt_PwANatrlIGrcEGw2B/view?usp=sharing" }
+                ]
+            },
+            {
+                title: "Ch.34 家族",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1vBEaeDn3lV8d5ozBSGvUhha-e5fKLB9c/view?usp=sharing" }
+                ]
+            },
+        ]
+    },
+    {
+        category: "日檢N5温習班",
+        categoryId: "jlpt-n5-revision-class",
+        topics: [
+            {
+                title: "單詞及動詞變化",
+                files: [
+                    { desc: "課堂筆記", url: "https://drive.google.com/file/d/1Rqr4WdScY5T8Ixyt553Z2M6fRof2PO2r/view?usp=drive_link" }
                 ]
             }
         ]
